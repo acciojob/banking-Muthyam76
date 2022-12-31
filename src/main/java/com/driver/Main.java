daead46b31e16da6461e92d6cbfd3c9bc95f1930
muthyam <dadigemuthyam@gmail.com>
2022-12-31 11:42:37 +0530

    L

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,10 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-     //   System.out.println("hello world");
-      // BankAccount a=new BankAccount("hh",500,0);
-       // System.out.println(a.generateAccountNumber(3,1));
+        System.out.println("hello world");
+       BankAccount a=new BankAccount("hh",500,0);
+        System.out.println(a.generateAccountNumber(3,28));
+        System.out.println("h");
 
     }
 }
